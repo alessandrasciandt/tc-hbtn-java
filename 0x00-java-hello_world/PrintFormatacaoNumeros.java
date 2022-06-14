a3b09777097572163464aea1383d4150437d8e62
Alessandra De Souza <107224377+alessandrasciandt@users.noreply.github.com>
2022-06-14 08:07:09 -0400

    Update PrintFormatacaoNumeros.java

--- 0x00-java-hello_world/PrintFormatacaoNumeros.java
+++ 0x00-java-hello_world/PrintFormatacaoNumeros.java
@@ -3,11 +3,12 @@ import java.util.Locale;
 
 public class PrintFormatacaoNumeros {
     public static void main(String[] args) {
-        Locale localeBR = new Locale("pt", "BR");
+        Locale localeBR = new Locale("en", "US");
         NumberFormat dinheiro = NumberFormat.getCurrencyInstance(localeBR);
+
         float taxa = 0.2456f;
         float valor = 7654.321f;
-        System.out.println("Valor: "+dinheiro.format(valor));
+        System.out.println("Valor: "+dinheiro.format(valor).replace("$","R$ "));
         System.out.println(String.format("Taxa: %02.2f%%", taxa));
     }
 }
