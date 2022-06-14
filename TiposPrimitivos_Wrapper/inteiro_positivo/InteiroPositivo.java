1d889d71466d7cc6e23575e1da75fb04bf3d8e4d
alessandrasciandt <alessandras@ciandt.com>
2022-06-14 14:50:04 -0400

    second commit Java Tipos primitivos

---
+++ TiposPrimitivos_Wrapper/inteiro_positivo/InteiroPositivo.java
@@ -0,0 +1,44 @@
+public class InteiroPositivo {
+    public int valor;
+    //public String valorString;
+
+    //constructor
+    public InteiroPositivo(Integer valor) {
+        if(valor.intValue() <= 0 || valor == null){
+            throw new IllegalArgumentException("Valor nao eh um valor inteiro positivo");
+        }
+        this.valor = valor;
+    }
+    public InteiroPositivo(String valor) {
+        if(Integer.parseInt(valor) <= 0 || valor == null){
+            throw new IllegalArgumentException("Valor nao eh um valor inteiro positivo");
+        }
+        this.valor = new Integer(valor);
+    }
+
+    //getter and setters
+    public Integer getValor() {
+        return valor;
+    }
+
+    public void setValor(Integer valor) {
+        if(valor.intValue() <= 0 || valor == null){
+            throw new IllegalArgumentException("Valor nao eh um valor inteiro positivo");
+        }else{
+            this.valor  = valor;
+        }
+    }
+
+
+    //metodo
+   public boolean ehPrimo(){
+        if (valor <= 1){
+            return false;
+        }
+      for (int i = 2; i < valor; i++){
+          if (valor % i == 0)
+              return false;
+      }
+      return true;
+   }
+}
