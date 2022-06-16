2f7a01cbac29d2fae952b416da469f3bbc5ab4d4
alessandrasciandt <alessandras@ciandt.com>
2022-06-16 12:08:12 -0400

    primeiro comite relacionamento entre classes

---
+++ 0x09/agregacao/Empregado.java
@@ -0,0 +1,48 @@
+public class Empregado {
+
+    private String nome;
+    private int codigo;
+    private Endereco endereco;
+
+    //construtores
+    public Empregado(String nome, int codigo, Endereco endereco) {
+        this.nome = nome;
+        this.codigo = codigo;
+        this.endereco = endereco;
+    }
+
+    //getters and setters
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public Endereco getEndereco() {
+        return endereco;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public void setEndereco(Endereco endereco) {
+        this.endereco = endereco;
+
+    }
+
+    public void apresentar(){
+        System.out.println("Codigo: "+this.getCodigo());
+        System.out.println("Nome: "+this.getNome());
+        System.out.println("Bairro: "+this.getEndereco().getBairro());
+        System.out.println("Cidade: "+this.getEndereco().getCidade());
+        System.out.println("Pais: "+this.getEndereco().getPais());
+    }
+}
