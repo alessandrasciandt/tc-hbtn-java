346ee1d5eb0b2a8bfec3d006b97b18e6bdd16055
alessandrasciandt <alessandras@ciandt.com>
2022-06-21 13:50:25 -0400

    primeiro comite Numeros de pares

---
+++ 0x01/pares/NumerosPares.java
@@ -0,0 +1,10 @@
+public class NumerosPares {
+
+    public static void main(String args[]) {
+        for (int i = 0; i <= 97; i++) {
+            if(i % 2 == 0){
+                System.out.printf(i + ", ");
+            }
+        } System.out.println("98");
+    }
+}
