46c356a07c3ff4452f7a1dd2c5143a01754763fa
alessandrasciandt <alessandras@ciandt.com>
2022-06-21 13:36:28 -0400

    primeiro comite Numeros de 0 a 99

--- 0x01/numeros/Numeros0a99.java
+++ 0x01/numeros/Numeros0a99.java
@@ -1,6 +1,5 @@
 public class Numeros0a99 {
 
-    public static Integer n = 99;
     public static void main(String args[]) {
         for (int i = 0; i <= 98; i++) {
             System.out.printf(i +", ");
