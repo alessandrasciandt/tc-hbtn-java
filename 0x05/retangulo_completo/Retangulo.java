c12335fb53ecbbe02ff05755485f4f7526ee0a72
alessandrasciandt <alessandras@ciandt.com>
2022-06-22 14:32:28 -0400

    primeiro comite Java retangulo_completo

---
+++ 0x05/retangulo_completo/Retangulo.java
@@ -0,0 +1,48 @@
+public class Retangulo extends FormaGeometrica{
+    private double largura;
+    private double altura;
+    @Override
+    public double area() {
+        double res = largura * altura;
+        return res;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("[Retangulo] %.2f / %.2f",largura, altura );
+    }
+
+    public Retangulo(){
+    }
+
+    public Retangulo(double largura, double altura) {
+        this.largura = largura;
+        this.altura = altura;
+    }
+
+    public double getLargura() {
+        return largura;
+    }
+
+    public void setLargura(double largura) {
+        this.largura = largura;
+        if(largura < 0){
+            throw new UnsupportedOperationException("Largura deve ser maior ou igual a 0");
+        }else{
+            this.largura = largura;
+        }
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+
+    public void setAltura(double altura) {
+        this.altura = altura;
+        if(altura < 0){
+            throw new UnsupportedOperationException("Altura deve ser maior ou igual a 0");
+        }else{
+            this.altura = altura;
+        }
+    }
+}
