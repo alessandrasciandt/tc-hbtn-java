bddc8c96a42558cd6d872970dc403256f1c8dcd3
alessandrasciandt <alessandras@ciandt.com>
2022-07-15 14:40:01 -0400

    comite Java – Streams, Collectors 0. Serialização de Estudantes (ajuste um)

--- Serializacao/serializacao/Estudante.java
+++ Serializacao/serializacao/Estudante.java
@@ -26,6 +26,6 @@ public class Estudante implements Serializable {
 
     @Override
     public String toString() {
-        return String.format("Estudante { nome='%s', idade='%d', senha='%d'}",nome, idade, senha);
+        return String.format("Estudante { nome='%s', idade='%d', senha='%d' }",nome, idade, senha);
     }
 }
